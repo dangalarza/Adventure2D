3134d1fb32279dd2d42116489fe0c941279595a8
danielZBwin <dnyl.glrza@gmail.com>
2025-12-17 23:33:40 -0600

    Added scriptable, directional player animation.

---
+++ Assets/Scenes/Scripts/Animation/CharacterAnimator.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    private Animator animator;
+    private Vector2 lastFacing = Vector2.down;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void UpdateAnimation(Vector2 movement)
+    {
+        float speed = movement.magnitude;
+
+        if (speed > 0)
+            lastFacing = movement.normalized;
+
+        string animName = GetAnimationName(lastFacing, speed);
+        animator.Play(animName);
+    }
+
+    private string GetAnimationName(Vector2 facing, float speed)
+    {
+        if (speed == 0)
+        {
+            if (facing.y > 0.5f) return "Idle_Up";
+            if (facing.y < -0.5f) return "Idle_Down";
+            if (facing.x < -0.5f) return "Idle_Left";
+            return "Idle_Right";
+        }
+        else
+        {
+            if (facing.y > 0.5f) return "Walk_Up";
+            if (facing.y < -0.5f) return "Walk_Down";
+            if (facing.x < -0.5f) return "Walk_Left";
+            return "Walk_Right";
+        }
+    }
+}
+
--- Assets/Scenes/Scripts/Player/PlayerController.cs
+++ Assets/Scenes/Scripts/Player/PlayerController.cs
@@ -2,41 +2,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Sprite spriteUp;
-    public Sprite spriteDown;
-    public Sprite spriteLeft;
-    public Sprite spriteRight;
-
-    private SpriteRenderer spriteRenderer;
     public float speed = 5f;
     public Vector2 FacingDirection { get; private set; } = Vector2.down;
-
     private Rigidbody2D rb2d;
     private Vector2 movement;
+    private CharacterAnimator characterAnimator;
 
     void Awake()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
         rb2d = GetComponent<Rigidbody2D>();
+        characterAnimator = GetComponent<CharacterAnimator>();
     }
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-
-        if (movement != Vector2.zero)
-        {
-            FacingDirection = movement.normalized;
-        }
-
         movement = movement.normalized;
+
         if (movement != Vector2.zero)
         {
-            FacingDirection = movement.normalized;
-            UpdateSpriteDirection();
+            FacingDirection = movement;
         }
+        movement = movement.normalized;
 
+
+        characterAnimator.UpdateAnimation(movement);
     }
 
     void FixedUpdate()
@@ -44,17 +35,4 @@ public class PlayerController : MonoBehaviour
         rb2d.linearVelocity = movement * speed;
     }
 
-    void UpdateSpriteDirection()
-{
-    if (FacingDirection.y > 0.5f)
-        spriteRenderer.sprite = spriteUp;
-    else if (FacingDirection.y < -0.5f)
-        spriteRenderer.sprite = spriteDown;
-    else if (FacingDirection.x < -0.5f)
-        spriteRenderer.sprite = spriteLeft;
-    else if (FacingDirection.x > 0.5f)
-        spriteRenderer.sprite = spriteRight;
 }
-
-}
-
