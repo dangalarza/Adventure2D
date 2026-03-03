862ab557bf3d00d7e8c2d681bdd20cda81fb9019
danielZBwin <dnyl.glrza@gmail.com>
2026-03-03 14:50:22 -0600

    Tweaked fairy's floating position

--- Assets/Scenes/Scripts/Player/FairyController.cs
+++ Assets/Scenes/Scripts/Player/FairyController.cs
@@ -6,6 +6,9 @@ public class FairyController : MonoBehaviour
     private float followDistance = 2f;
     private float moveSpeed = 3f;
 
+    [SerializeField] private float y_offset = .8f;
+
+
     public void Initialize(Transform player)
     {
         playerTransform = player;
@@ -22,6 +25,8 @@ public class FairyController : MonoBehaviour
     private void FollowPlayer()
     {
         Vector3 targetPosition = playerTransform.position - playerTransform.forward * followDistance;
+        targetPosition.y += y_offset;
+
         transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
     }
 }
